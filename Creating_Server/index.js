52ca88ad66b6723c753589ab01e8ee1118e83dec
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-06-25 22:17:54 +0530

    Adding

---
+++ Creating_Server/index.js
@@ -0,0 +1,26 @@
+const { log } = require('console');
+const http = require('http');
+
+const myServer = (req, res)=>{
+    console.log("Request received");
+    console.log(req.url); //where request received
+    const response = {
+        success : true,
+        message : 'This is my first API'
+    }
+    if(req.url === "/login"){
+        res.end(JSON.stringify(response))
+    }else if(req.url === "/logout"){
+        res.end("Logout API called")
+    }else if(res.url === "/user-list"){
+        res.end("User-List Api called")
+    }
+
+
+
+}
+
+const server = http.createServer(myServer);
+server.listen(1000,()=>{
+    console.log("Server is up and running at port 1000");
+})
