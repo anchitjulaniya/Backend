38fa3d461d16231a4a02809417dc100cecd59a4c
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-07-16 17:26:04 +0530

    adding env

--- ProjectJob/index.js
+++ ProjectJob/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const dotenv = require('dotenv');
 const app = express();
 const port = 10000;
-
+dotenv.config();
 const jobRoutes = require('./Routes/job')
 
 // mongoose.connect('mongodb://localhost:27017/Fs-16-17')
-mongoose.connect('mongodb+srv://anchitjulaniyaofficial:KFBsUkeTxvHRuxAL@cluster0.ca1kgtk.mongodb.net/')
+// mongoose.connect('mongodb+srv://anchitjulaniyaofficial:KFBsUkeTxvHRuxAL@cluster0.ca1kgtk.mongodb.net/')
+console.log(process.env.DATABASE_URL);
+mongoose.connect(process.env.DATABASE_URL)
 .then(()=>console.log("MongoDB Connected Successfully"))
 .catch((error)=>{console.log("MongoBD Error", error);})
 
