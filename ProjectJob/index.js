2a9879372a87df129b9865ba2e03d7255fa6d8a4
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-07-16 16:34:47 +0530

    adding mongoose url

--- ProjectJob/index.js
+++ ProjectJob/index.js
@@ -6,7 +6,8 @@ const port = 10000;
 
 const jobRoutes = require('./Routes/job')
 
-mongoose.connect('mongodb://localhost:27017/Fs-16-17')
+// mongoose.connect('mongodb://localhost:27017/Fs-16-17')
+mongoose.connect('mongodb+srv://anchitjulaniyaofficial:KFBsUkeTxvHRuxAL@cluster0.ca1kgtk.mongodb.net/')
 .then(()=>console.log("MongoDB Connected Successfully"))
 .catch((error)=>{console.log("MongoBD Error", error);})
 
