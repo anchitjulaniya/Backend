0eeb82ed3cc99b7e28913bfe3b45bce51f448bdc
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-07-25 13:36:18 +0530

    updating mongodb link

--- BloggingApp/index.js
+++ BloggingApp/index.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require('mongoose')
 const routes = require('./Routes/post')
 
-mongoose.connect('mongodb://localhost:27017/BloggingApp')
+mongoose.connect('mongodb+srv://anchitjulaniyaofficial:KFBsUkeTxvHRuxAL@cluster0.ca1kgtk.mongodb.net/BloggingApp')
 .then(()=>{console.log('DB connected Successfully')})
 .catch(()=>{console.log('Mongoose Error')})
 
